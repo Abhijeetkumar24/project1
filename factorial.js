93440ff27049dcd1f5b635058d4eb0f6f6bf07e2
abhijeet <abhijeet.kumar@appinventiv.com>
2023-06-28 10:11:58 +0530

    updated

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+  
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const number = 5;
+const result = factorial(number);
+console.log(result);
+
